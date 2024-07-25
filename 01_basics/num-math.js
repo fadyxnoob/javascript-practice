fc1769c2a828785d9b7f97025373e33671b2708a
fadyxnoob <danishmehbob886@gmail.com>
2024-07-25 14:27:29 +0500

    add math and numb

---
+++ 01_basics/num-math.js
@@ -0,0 +1,33 @@
+// const balance = new Number(200);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 120.73345
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 10000000
+
+// console.log(hundreds.toLocaleString());
+
+
+// ++++++++++++++++ MATH +++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.2));
+// console.log(Math.min(1,5,10,9,2,0));
+// console.log(Math.max(0,1,3,5,9,70,10));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) +1);
+// console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+const value = Math.floor(Math.random() * (max - min + 1) + min)
+console.log(`The number value is ${value}`);
+
