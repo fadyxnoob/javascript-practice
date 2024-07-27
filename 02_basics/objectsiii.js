fa7580c37c293a08bc7df8d66f22cb9efb9bc110
fadyxnoob <danishmehbob886@gmail.com>
2024-07-27 11:31:02 +0500

    objects ends

---
+++ 02_basics/objectsiii.js
@@ -0,0 +1,10 @@
+// ojects destructring
+const course = {
+    courseName : "js in hindi",
+    coursePrise : 999,
+    courseInstructure : "Hetish" 
+}
+
+// destrustring 
+const {courseInstructure : instructure} = course
+console.log(instructure);
